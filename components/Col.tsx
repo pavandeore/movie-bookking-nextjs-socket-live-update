538d9557715bdd1395489615c1c2998ba2f30cfd
user d <userd@gmail.com>
2022-11-26 22:00:43 +0530

    seat booked basic done

--- components/Col.tsx
+++ components/Col.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+
+import io from 'socket.io-client'
+let socket
 
 import  { useAtom } from 'jotai'
 import { DATAAtom, flatDATAAtom } from '../atom/DATAAtom'
@@ -6,7 +10,27 @@ import { DATAAtom, flatDATAAtom } from '../atom/DATAAtom'
 
 const Col = ({col}) => {
 
+
+    const [seat, setSeat] = useState(null)
     const [dataAtom, setDataAtom] = useAtom(flatDATAAtom)
+
+    useEffect(() => {
+        socketInitializer()
+    }, [])
+
+    const socketInitializer = async () => {
+        await fetch('/api/socket');
+        socket = io()
+
+        socket.on('connect', () => {
+            console.log('connected')
+        })
+
+        socket.on('seat-booked', msg => {
+            seatBookHandler(msg)
+        })
+    }
+    
 
     let seatStyles;
     if(col.is_booked){
@@ -15,24 +39,30 @@ const Col = ({col}) => {
         seatStyles = 'bg-gray-200'
     }
 
-
-
-    const seatBookHandler = (col) =>{
-    
-
+    const clickHandler = (ID) => {
         dataAtom.forEach( data => {
-            if(data.id==col.id){
+            if(data.id==ID){
+                setDataAtom( [ ...dataAtom, data.is_booked ? data.is_booked = false : data.is_booked = true ] )
+                socket.emit('seat-booked', ID)
+            }
+        })
+    }
+
+
+    const seatBookHandler = (ID) =>{
+        dataAtom.forEach( data => {
+            if(data.id==ID){   
                 setDataAtom( [ ...dataAtom, data.is_booked = true ] )
             }
         })
-
     }
+    
     
 
   return (
     
     
-    <button onClick={() => seatBookHandler(col)} className={'h-10 w-10 mt-10 cursor-pointer flex pt-2 justify-center '+ seatStyles} > 
+    <button onClick={() => clickHandler(col.id)} className={'h-10 w-10 mt-10 cursor-pointer flex pt-2 justify-center '+ seatStyles} > 
         {col.id} 
     </button>
 
